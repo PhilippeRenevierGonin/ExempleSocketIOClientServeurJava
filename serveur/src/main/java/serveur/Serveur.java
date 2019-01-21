a0c8a7d9d8b27227541c8d73eb9a1e066e8de775
Philippe Renevier Gonin <philippe.renevier@unice.fr>
2019-01-21 22:57:57 +0100

    une ébauche de serveur qui attend une connexion

---
+++ serveur/src/main/java/serveur/Serveur.java
@@ -0,0 +1,64 @@
+package serveur;
+
+import com.corundumstudio.socketio.Configuration;
+import com.corundumstudio.socketio.SocketIOClient;
+import com.corundumstudio.socketio.SocketIOServer;
+import com.corundumstudio.socketio.listener.ConnectListener;
+
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+
+/**
+ * attend une connexion puis s'arrête
+ */
+public class Serveur {
+
+
+    public static final void main(String []args) {
+        try {
+            System.setOut(new PrintStream(System.out, true, "UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        Configuration config = new Configuration();
+        config.setHostname("127.0.0.1");
+        config.setPort(10101);
+
+        // creation du serveur
+        SocketIOServer server = new SocketIOServer(config);
+        server.start();
+
+        // Objet de synchro
+        final Object attenteConnexion = new Object();
+
+        System.out.println("préparation du listener");
+
+        // on accept une connexion
+        server.addConnectListener(new ConnectListener() {
+            public void onConnect(SocketIOClient socketIOClient) {
+                System.out.println("connexion de "+socketIOClient.getRemoteAddress());
+                attenteConnexion.notify();
+            }
+        });
+
+
+        System.out.println("en attente de connexion");
+        synchronized (attenteConnexion) {
+            try {
+                attenteConnexion.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                System.err.println("erreur dans l'attente");
+            }
+        }
+
+        System.out.println("Une connexion est arrivée, on arrête");
+        server.stop();
+
+
+
+        System.out.println("fin du main");
+
+    }
+}
