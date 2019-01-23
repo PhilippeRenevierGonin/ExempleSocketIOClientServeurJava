8b1091d2c2d1b1d535ca5b6b781bd621bdc583b6
Philippe Renevier Gonin <philippe.renevier@unice.fr>
2019-01-23 09:56:53 +0100

    ajout d'un lanceur pour faire un client et un serveur. ajout d'un peu de javadoc (pom parent + sur la classe Coup)

--- commun/src/main/java/commun/Coup.java
+++ commun/src/main/java/commun/Coup.java
@@ -20,7 +20,16 @@ public class Coup {
         this.plusGrand = plusGrand;
     }
 
+    /**
+     *  constructeur par défaut pour le mapping JSOn - Objet java
+     */
     public Coup() {}
+
+    /**
+     * constructeur pour créer un coup joué avec la réponse (si ce n'est pas la bonne)
+     * @param val la valeur qui a été proposée par le.a joueur.se
+     * @param sup indique si la valeur est plus grande que le nombre recherché
+     */
     public Coup(int val, boolean sup) {
         setCoup(val);
         setPlusGrand(sup);
