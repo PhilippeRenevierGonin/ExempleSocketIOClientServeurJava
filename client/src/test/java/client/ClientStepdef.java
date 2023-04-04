5cc86a1df510d14381e7d03903ad98f1f0c0de8f
PhilippeRenevierGonin <philippe.renevier@unice.fr>
2023-04-04 09:19:58 +0200

    mise à jour de spring pour le serveur et de junit/mockito pour le client

--- client/src/test/java/client/ClientStepdef.java
+++ client/src/test/java/client/ClientStepdef.java
@@ -2,11 +2,9 @@ package client;
 
 import client.reseau.ConnexionClient;
 import client.vue.Vue;
-import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.mockito.Mock;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyInt;
